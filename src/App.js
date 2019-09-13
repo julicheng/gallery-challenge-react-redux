4b51c24517987fe18d33ec9e3255eb09545067e7
Juli Cheng <julicheng@Julis-MacBook-Pro.local>
2019-09-13 20:58:24 +0100

    Print item id to itemdetail page

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Header from "./components/Header";
 import Gallery from "./components/Gallery";
 import Footer from "./components/Footer";
 import ItemDetail from "./components/ItemDetail";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 const App = () => {
   return (
--- src/components/ItemDetail.js
+++ src/components/ItemDetail.js
@@ -1,16 +1,9 @@
 import React from "react";
 
-const ItemDetail = ({ match, location }) => {
+const ItemDetail = ({ match }) => {
   return (
     <>
-      <p>
-        <strong>Match Props: </strong>
-        <code>{JSON.stringify(match, null, 2)}</code>
-      </p>
-      <p>
-        <strong>Location Props: </strong>
-        <code>{JSON.stringify(location, null, 2)}</code>
-      </p>
+      <p>{match.params.itemid}</p>
     </>
   );
 };
