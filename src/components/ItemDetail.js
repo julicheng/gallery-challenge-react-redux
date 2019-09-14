fb570701083be1586ac4cd930686c4eb4948b8de
Juli Cheng <julicheng@Julis-MacBook-Pro.local>
2019-09-14 15:18:17 +0100

    Display values on item page

--- src/components/ItemDetail.js
+++ src/components/ItemDetail.js
@@ -1,11 +1,41 @@
-import React from "react";
+import React, { Component } from "react";
 
-const ItemDetail = ({ match }) => {
-  return (
-    <>
-      <p>{match.params.itemid}</p>
-    </>
-  );
-};
+class ItemDetail extends Component {
+  state = { title: "", description: "", url: "" };
+
+  componentDidMount() {
+    const itemId = this.props.match.params.itemid;
+    fetch(
+      `http://axielldevtest.eastus2.cloudapp.azure.com:3000/api/media/` + itemId
+    )
+      .then(response => response.json())
+      .then(this.printItems)
+      .catch(function() {
+        console.log("Error getting items");
+      });
+  }
+
+  printItems = data => {
+    this.setState({
+      title: data.title,
+      description: data.description,
+      url: data.url
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <div>
+          <img src={this.state.url} alt={this.state.title} />
+        </div>
+        <div>
+          <h2>{this.state.title}</h2>
+          <p>{this.state.description}</p>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default ItemDetail;
