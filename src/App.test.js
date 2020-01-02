da8c1a7f88223bcd9df63944f1f0178ab7ff3e51
Juli Cheng <julicheng@julismbpaxiell.lan>
2020-01-02 17:53:04 +0000

    Add test to header component

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,51 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import { shallow, mount, render } from "enzyme";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Filter from "./components/Filter";
+import Item from "./components/Item";
+import App from "./App";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+// console.log(component.debug());
+
+// const setUp = (props={}) => {
+//   const component = shallow(He)
+// }
+
+describe("Header Component", () => {
+  it("Should render without errors", () => {
+    const component = shallow(<Header />);
+    const wrapper = component.find("header img.logo");
+    expect(wrapper.length).toBe(1);
+  });
 });
+
+// describe("Footer Component", () => {
+//   it("Should render without errors", () => {
+//     const component = shallow(<Footer />);
+//     const wrapper = component.find("footer h3");
+//     expect(wrapper.length).toBe(1);
+//   });
+// });
+
+// describe("Filter Component", () => {
+//   it("Should render without errors", () => {
+//     const props = {
+//       filter: "abstract",
+//       handleFilterChange: "#"
+//     };
+//     const component = shallow(<Filter props={props} />);
+//     console.log(component.debug());
+//     const wrapper = component.find("select");
+//     expect(wrapper.length).toBe(1);
+//   });
+// });
+
+// describe("Item Component", () => {
+//   it("It should render without errors", () => {
+//     const component = shallow(<Item />);
+//     console.log(component.debug());
+//     const wrapper = component.find("div");
+//     expect(wrapper.length).toBe(1);
+//   });
+// });
